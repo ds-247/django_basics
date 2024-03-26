bdb257151f0069af6f42ce5761fb82ec4a511ad5
Cipher <dipanshusaini69@gmail.com>
2024-03-27 02:46:09 +0530

    showed only interested fields to table , and display the actual name of genre instead of object 1 in table

--- django_mosh/03_vidly/movies/admin.py
+++ django_mosh/03_vidly/movies/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Genre, Movies
+
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name');
+
+class MovieAdmin(admin.ModelAdmin):
+    exclude = ('date_created', )
+    list_display = ('id', 'title', 'daily_rate')
 
 # Register your models here.
+admin.site.register(Genre, GenreAdmin);
+admin.site.register(Movies, MovieAdmin)
