ad755ecb35235d5f3fa31d4fdcf5ee5032b97472
Cipher <dipanshusaini69@gmail.com>
2024-03-27 02:56:03 +0530

    displayed the movies name instead of the hello world

--- django_mosh/03_vidly/movies/views.py
+++ django_mosh/03_vidly/movies/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Movies
 
 # Create your views here.
 def  index(request) :
-    return HttpResponse("Hello World");
+    movies = Movies.objects.all();
+    movie_names = ', '.join([m.title for m in movies])
+    return HttpResponse(movie_names);
