9f5c85449b32cf6826c94da197129ab228c75d96
Cipher <dipanshusaini69@gmail.com>
2024-03-27 01:32:28 +0530

    created routes , and added models

--- django_mosh/03_vidly/movies/models.py
+++ django_mosh/03_vidly/movies/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Genre(models.Model) :
+    name = models.CharField(max_len=255);
+
+class Movies(models.Model) :
+    title = models.CharField(max_len = 255);
+    release_year = models.IntegerField();
+    number_in_stock = models.IntegerField();
+    daily_rate = models.FloatField();
+    genre = models.ForeignKey(Genre, on_delete = models.CASCADE)
